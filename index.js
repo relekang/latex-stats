4488981721dbfc28190aab5020cfb487fe7e9691
Rolf Erik Lekang <me@rolflekang.com>
2014-10-08 16:31:09 +0200

    move cli stuff to cli.bs

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-var program = require("commander");
-var stats = require("./stats");
-var createGraphData = require("./createGraphData");
+#!/usr/bin/env node
 
-var version = require('./package.json').version;
-
-
-program.version(version).usage('<command> (<working-dir> <filename>)').parse(process.argv);
-
-if (!program.args[0]) {
-    console.log('Missing command');
-    process.exit(1);
-}
-
-if (program.args[0] === 'log') {
-    if (!program.args[1]) {
-        console.log('Missing path');
-        process.exit(1);
-    }
-
-    if (!program.args[2]) {
-        console.log('Missing filename');
-        process.exit(1);
-    }
-
-    stats(program.args[1], program.args[2]);
-} else if (program.args[0] === 'graph') {
-    createGraphData(function() {
-        process.exit(0);
-    });
-}
-
-
-module.exports = program;
+require('./cli');
